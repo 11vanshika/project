469aff66a246320262925169d1c8734175b4a3c4
11vanshika <113531418+11vanshika@users.noreply.github.com>
2022-09-15 19:45:30 +0530

    Add files via upload

---
+++ project/project/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/* 1. Write a C# Sharp program that takes three letters as input and display them in reverse order.
+          */
+            /*char letter1 = Convert.ToChar(Console.ReadLine());
+            char letter2 = Convert.ToChar(Console.ReadLine());
+            char letter3= Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(letter3);
+            Console.WriteLine(letter2);
+            Console.WriteLine(letter1);
+            //Console.WriteLine(letter3,letter2 , letter1)*/
+
+            /*2.Write a C# Sharp program that takes a number and a width also a number,
+                as input and then displays a triangle of that width, using that number */
+
+            /*int height =Convert.ToInt32(Console.ReadLine());
+             int width =Convert.ToInt32(Console.ReadLine());
+             for(int i = height; i>0; i--)
+             {
+                 for(int j = i; j > 0; j--)
+                 {
+                     Console.Write(width);
+                 }
+                 Console.WriteLine();
+             }*/
+
+
+            /* 4.Write a C# Sharp program that takes two numbers as input and perform an operation (+,-,*,x,/) 
+                 on them and displays the result of that operation*/
+            /* int  first = Convert.ToInt32(Console.ReadLine());
+             int second = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine(first + second);
+             Console.WriteLine(first - second);
+             Console.WriteLine(first * second);
+             Console.WriteLine(first / second);*/
+
+
+            /* 5. Write a C# Sharp program that takes the radius of a 
+             circle as input and calculate the perimeter and area of the circle.
+             */
+            /* int radious = Convert.ToInt32(Console.ReadLine());
+             double pi = 3.14;
+             double perimeter = (2 * pi * radious);
+             Console.WriteLine(perimeter); */
+
+
+
+        }
+
+    }
+}
+
+
